820dfad045276fbbf4dc815d589349bda32c551d
muschellij2 <muschellij2@gmail.com>
2020-10-01 10:47:15 -0400

    changed some depends

--- tests/testthat/test-train_model.R
+++ tests/testthat/test-train_model.R
@@ -1,7 +1,9 @@
 test_that("training", {
   Sys.setlocale('LC_ALL', 'C')
   on.exit(Sys.setlocale("LC_CTYPE", ""))
-  output = expect_warning(train_model(dcm_dir='../../../dcm_test', gt_labels_path='../../../gt_labels.csv', n_cv = 2, repeats = 2, save_path = FALSE))
+  output = expect_warning(train_model(dcm_dir='../../../dcm_test',
+                                      gt_labels_path='../../../gt_labels.csv', n_cv = 2,
+                                      repeats = 2, save_path = FALSE))
   expect_type(output, 'list')
   expect_s3_class(output$models[[1]], 'train')
   expect_known_hash(output$tb, '45b3cf6da0')
